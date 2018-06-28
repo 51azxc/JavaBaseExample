474f9c620a8ab9f7cd7c81d5889f98ac7df1d54e
Hu <hu@HudeMacBook-Pro.local>
2018-06-29 01:29:23 +0800

    finished reactor test

--- demo-javase/src/main/java/thirdparty/ReactorTest.java
+++ demo-javase/src/main/java/thirdparty/ReactorTest.java
@@ -3,12 +3,14 @@ package thirdparty;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /*
  * 反应式流（Reactive Streams）
@@ -27,12 +29,14 @@ import reactor.core.publisher.Mono;
 public class ReactorTest {
 
 	public static void main(String[] args) {
-		//System.out.println("----------创建Flux----------");
-		//createFlux();
-		//System.out.println("----------创建Mono----------");
-		//createMono();
+		System.out.println("----------创建Flux----------");
+		createFlux();
+		System.out.println("----------创建Mono----------");
+		createMono();
 		System.out.println("----------操作Flux----------");
 		operateFlux();
+		System.out.println("----------消息处理----------");
+		handleMessage();
 	}
 
 	//创建Flux
@@ -41,19 +45,23 @@ public class ReactorTest {
 		//简单的序列使用静态方法创建
 		//just()方法指定序列中包含的全部元素。创建出来的 Flux 序列在发布这些元素之后会自动结束。
 		Flux.just("Hello", "World").subscribe(i -> System.out.print(i + " "));
+		
 		System.out.println("\n----------flux fromArray----------");
 		//fromArray()/fromIterable()/fromStream()方法可以从数组/迭代对象/Stream中创建
 		Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(i -> System.out.print(i + " "));
+		
 		System.out.println("\n----------flux empty----------");
 		//empty()方法创建一个不包含任何元素，只发布结束消息的序列。
 		Flux.empty().subscribe(System.out::println);
+		
 		System.out.println("\n----------flux range----------");
 		//range(start, count)方法创建包含从 start起始的 count个数量的 Integer对象的序列。
 		Flux.range(1, 10).subscribe(i -> System.out.print(i + " "));
+		
 		System.out.println("\n----------flux interval----------");
 		//interval(period)方法创建一个包含了从 0 开始递增的Long对象的序列。其中包含的元素按照指定的间隔来发布。
 		//interval(delay, period)方法创建一个除了间隔时间之外，还可以指定起始元素发布之前的延迟时间的递增序列。
-		Flux.interval(Duration.of(1, ChronoUnit.SECONDS)).subscribe(i -> System.out.print(i + " "));
+		Flux.interval(Duration.of(1, ChronoUnit.SECONDS)).take(5).toStream().forEach(i -> System.out.print(i + " "));
 		
 		System.out.println("\n----------flux generate----------");
 		//generate()方法通过同步和逐一的方式来产生Flux序列。
@@ -115,9 +123,11 @@ public class ReactorTest {
 		//buffer会将流中的元素收集到集合中，并把集合对象作为流中的新元素
 		//buffer()方法把流中10个数据元素分成3个数组
 		Flux.range(1, 10).buffer(3).subscribe(System.out::println);
+		
 		System.out.println("----------bufferUntil----------");
 		//bufferUntil会一直存放元素直到条件为true。这里就是每次碰到偶数就分割一个数组到流中
 		Flux.range(1, 10).bufferUntil(i -> i % 2 == 0).subscribe(System.out::println);
+		
 		System.out.println("----------bufferWhile----------");
 		//bufferWhile是只当条件为true时就存放那个元素，这里就是只存放偶数的数组流
 		Flux.range(1, 10).bufferWhile(i -> i % 2 == 0).subscribe(System.out::println);
@@ -139,12 +149,15 @@ public class ReactorTest {
 		System.out.println("----------take----------");
 		//take()方法用来从当前流中提取元素
 		Flux.range(1, 10).take(2).subscribe(System.out::println);
+		
 		System.out.println("----------takeLast----------");
 		//takeLast(n)方法用来提取最后n个元素
 		Flux.range(1, 10).takeLast(2).subscribe(System.out::println);
+		
 		System.out.println("----------takeUntil----------");
 		//takeUntil()方法是提取元素直到条件为true
 		Flux.range(1, 10).takeUntil(i -> i == 5).subscribe(System.out::println);
+		
 		System.out.println("----------takeWhile----------");
 		//takeWhile()方法是当条件为true时才提取元素
 		Flux.range(1, 10).takeWhile(i -> i < 5).subscribe(System.out::println);
@@ -157,17 +170,103 @@ public class ReactorTest {
 		
 		System.out.println("----------merge----------");
 		//merge可以将两个流合并成一个流，顺序为元素实际产生的顺序
-		Flux.merge(Flux.interval(Duration.ofMillis(10)).take(5), Flux.interval(Duration.ofMillis(20)).take(5))
+		//第一个流每隔10ms产生一个元素，取前5个；第二个流会先延迟5ms才开始产生元素
+		Flux.merge(Flux.interval(Duration.ofMillis(10)).take(5), 
+				Flux.interval(Duration.ofMillis(5), Duration.ofMillis(10)).take(5))
 			.toStream().forEach(i -> System.out.print(i + " "));
 		System.out.println();
+		
 		//mergeSequential按照所有流被订阅的顺序，以流为单位进行合并
-		Flux.mergeSequential(Flux.interval(Duration.ofMillis(10)).take(5), Flux.interval(Duration.ofMillis(20)).take(5))
+		Flux.mergeSequential(Flux.interval(Duration.ofMillis(10)).take(5), 
+				Flux.interval(Duration.ofMillis(5), Duration.ofMillis(10)).take(5))
 			.toStream().forEach(i -> System.out.print(i + " "));
 		
 		System.out.println("\n----------flatMap----------");
 		//flatMap是将流中的每个元素转换成一个流，然后再进行合并，类似拍平效果。
-		Flux.just(5, 10).flatMap(x -> Flux.interval(Duration.ofMillis(10))
+		//每次延时的失效会慢慢变大
+		Flux.just(5, 10).flatMap(x -> Flux.interval(Duration.ofMillis(x * 10), Duration.ofMillis(10))
 			.take(x)).toStream().forEach(i -> System.out.print(i + " "));
+		System.out.println();
+		//flatMapSequential则是按订阅顺序合并流,合并之前就已经订阅流所有的流
+		Flux.just(5, 10).flatMapSequential(x -> 
+			Flux.interval(Duration.ofMillis(x * 10), Duration.ofMillis(10))
+				.take(x)).toStream().forEach(i -> System.out.print(i + " "));
 		
+		System.out.println("\n----------concatMap----------");
+		//concatMap跟flatMap功能类似，不同的是它是按原始流中元素顺序进行转换合并，
+		//与flatMapSequential不同的是它对转换之后的流的订阅是动态进行的
+		Flux.just(5, 10).concatMap(x -> 
+			Flux.interval(Duration.ofMillis(x * 10), Duration.ofMillis(10))
+				.take(x)).toStream().forEach(i -> System.out.print(i + " "));
+		System.out.println();
+			
+		System.out.println("\n----------combineLatest----------");
+		//combineLatest把所有流中最新产生的元素合并到一起返回到结果流中
+		//这里最新产生的元素会放入到数组中，最后通过Arrays.toString方法输出
+		Flux.combineLatest(Arrays::toString,
+			Flux.interval(Duration.ofMillis(10)).take(5),
+			Flux.interval(Duration.ofMillis(5), Duration.ofMillis(10)).take(5)
+		).toStream().forEach(System.out::println);
 	}
+	
+	//消息处理
+	public static void handleMessage() {
+		System.out.println("----------subscribe----------");
+		//通过 subscribe()方法处理正常和错误消息
+		Flux.just(1, 2)
+			.concatWith(Mono.error(new IllegalStateException()))
+			.subscribe(System.out::println, System.err::println);
+		
+		System.out.println("----------error return----------");
+		//当出现错误时,通过onErrorReturn()方法返回一个默认值
+		Flux.just(1, 2)
+			.concatWith(Mono.error(new IllegalStateException()))
+			.onErrorReturn(0)
+			.subscribe(System.out::println);
+		
+		System.out.println("----------error resume----------");
+		//当出现错误时,通过onErrorResume()方法来根据不同的异常类型来选择要使用的产生元素的流
+		Flux.just(1, 2)
+        	.concatWith(Mono.error(new IllegalStateException()))
+        	.onErrorResume(e -> {
+        		if (e instanceof IllegalStateException) {
+                    return Mono.just(0);
+                } else if (e instanceof IllegalArgumentException) {
+                    return Mono.just(-1);
+                }
+        		return Mono.empty();
+        	})
+        	.subscribe(System.out::println);
+		
+		System.out.println("----------error retry----------");
+		//当出现错误时,通过retry操作符来进行重试
+		Flux.just(1, 2)
+        	.concatWith(Mono.error(new IllegalStateException()))
+        	.retry(1)
+        	.onErrorReturn(0)
+        	.subscribe(System.out::println);
+		
+		//通过调度器让流在不同的线程上执行操作
+		Flux.create(sink -> {
+			sink.next(Thread.currentThread().getName());
+			sink.complete();
+		})
+		.publishOn(Schedulers.single())		//切换为单线程
+		.map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
+		.publishOn(Schedulers.elastic())	//切换为线程池
+		.map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
+		.subscribeOn(Schedulers.parallel())	//切换为并行线程池
+		.toStream()
+		.forEach(System.out::println);
+		
+		Flux.range(1, 10).parallel(2)
+			.subscribe(i -> 
+				System.out.println(Thread.currentThread().getName() + " -> " + i));
+
+		Flux.range(1, 10).parallel(2)
+			.runOn(Schedulers.parallel())
+			.subscribe(i -> 
+				System.out.println(Thread.currentThread().getName() + " -> " + i));
+	}
+	
 }
--- demo-javase/src/test/java/thirdparty/reactive/ReactorTest.java
+++ demo-javase/src/test/java/thirdparty/reactive/ReactorTest.java
@@ -1,13 +1,9 @@
 package thirdparty.reactive;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
 import org.junit.Test;
 
@@ -15,152 +11,77 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
 
 public class ReactorTest {
 	
 	@Test
-	public void testFluxCreate1() {
-		System.out.println("---just---");
-		Flux.just("Hello", "World").subscribe(r -> System.out.print(r + " "));
-		System.out.println("\n---from array---");
-		Flux.fromArray(new Integer[] { 1, 2, 3 }).subscribe(System.out::print);
-		System.out.println("\n---empty---");
-		Flux.empty().subscribe(System.out::println);
-		System.out.println("---range---");
-		Flux.range(1, 10).subscribe(System.out::print);
-		System.out.println("\n---interval---");
-		Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
-	}
+	public void test1() {
+		//使用StepVerifier验证流中的元素
+		//expectNext()方法测试所期待的流中的下一个元素的值
+		//verifyComplete()方法则验证流是否正常结束
+		StepVerifier.create(Flux.just("a", "b"))
+        	.expectNext("a")
+        	.expectNext("b")
+        	.verifyComplete();
+		
+		StepVerifier.create(Flux.just(1, 3, 5, 2, 4, 6, 11, 12, 13)
+								.groupBy(i -> i % 2 == 0 ? "even" : "odd")
+								.concatMap(g -> g.defaultIfEmpty(-1)
+								.map(String::valueOf)
+						        .startWith(g.key()))
+		).expectNext("odd", "1", "3", "5", "11", "13")
+		 .expectNext("even", "2", "4", "6", "12")
+		 .verifyComplete();
+		
+		StepVerifier.create(Flux.range(1, 10).window(5, 3)
+				.concatMap(g -> g.defaultIfEmpty(-1))
+		).expectNext(1, 2, 3, 4, 5)
+		 .expectNext(4, 5, 6, 7, 8)
+		 .expectNext(7, 8, 9, 10)
+		 .expectNext(10)
+		 .verifyComplete();
 
-	@Test
-	public void testFluxCreate2() {
-		Flux.generate(sink -> {
-			sink.next("Hello");
-			sink.complete();
-		}).subscribe(System.out::println);
-
-		final Random random = new Random();
-		Flux.generate(ArrayList::new, (list, sink) -> {
-			int value = random.nextInt(100);
-			list.add(value);
-			sink.next(list);
-			if (list.size() == 10) {
-				sink.complete();
-			}
-			return list;
-		}).subscribe(System.out::println);
-
-		Flux.create(sink -> {
-			for (int i = 0; i < 10; i++) {
-				sink.next(i);
-			}
-			sink.complete();
-		}).subscribe(s -> System.out.print(s + " "));
-	}
-
-	@Test
-	public void testMonoCreate() {
-		Mono.fromSupplier(() -> "Hello").subscribe(System.out::println);
-		Mono.justOrEmpty(Optional.of("Hello")).subscribe(System.out::println);
-		Mono.create(sink -> sink.success("Hello")).subscribe(System.out::println);
-	}
-
-	@Test
-	public void testFluxOperator1() {
-		Flux.range(1, 100).buffer(20).subscribe(System.out::println);
-		Flux.range(1, 10).bufferUntil(i -> i % 2 == 0).subscribe(System.out::println);
-		Flux.range(1, 10).bufferWhile(i -> i % 2 == 0).subscribe(System.out::println);
-		Flux.range(1, 10).filter(i -> i % 2 == 0).subscribe(System.out::println);
-
-		Flux.range(1, 100).window(20).take(2).toStream().forEach(System.out::println);
-	}
-
-	@Test
-	public void testFluxOperator2() {
-		Flux.just("a", "b").zipWith(Flux.just("c", "d")).subscribe(System.out::println);
-		Flux.just("a", "b").zipWith(Flux.just("c", "d"), (s1, s2) -> String.format("%s-%s", s1, s2))
-				.subscribe(System.out::println);
-
-		Flux.range(1, 10).take(1).subscribe(System.out::println);
-		Flux.range(1, 10).takeLast(1).subscribe(System.out::println);
-		Flux.range(1, 10).takeWhile(i -> i < 4).subscribe(System.out::print);
-		System.out.println("\n----");
-		Flux.range(1, 10).takeUntil(i -> i < 4).subscribe(System.out::print);
-		System.out.println("\n----");
-		Flux.range(1, 100).reduce((x, y) -> x + y).subscribe(System.out::println);
-		Flux.range(1, 100).reduceWith(() -> 100, (x, y) -> x + y).subscribe(System.out::println);
-	}
-
-	@Test
-	public void testFluxOperator3() {
-		// Flux.just(5,10).flatMap(x -> Flux.interval(Duration.of(x,
-		// ChronoUnit.SECONDS)).take(x)).toStream().forEach(System.out::println);
-		Flux.just(1, 2).concatWith(Mono.error(new IllegalStateException())).retry(1).onErrorReturn(0)
-				.subscribe(System.out::println, System.err::println);
-
-		Flux.create(sink -> {
-			sink.next(Thread.currentThread().getName());
-			sink.complete();
-		}).publishOn(Schedulers.single()).map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
-				.publishOn(Schedulers.elastic()).map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
-				.subscribeOn(Schedulers.parallel()).toStream().forEach(System.out::println);
-
-		StepVerifier.create(Flux.just("a", "b")).expectNext("a").expectNext("b").verifyComplete();
+		StepVerifier.create(Flux.range(1, 10).buffer(5, 3))
+			.expectNext(Arrays.asList(1, 2, 3, 4, 5))
+			.expectNext(Arrays.asList(4, 5, 6, 7, 8))
+			.expectNext(Arrays.asList(7, 8, 9, 10))
+			.expectNext(Collections.singletonList(10))
+			.verifyComplete();
 
 	}
-
-	private static List<String> words = Arrays.asList("the", "quick", "brown", "fox", "jumped", "over", "the", "lazy",
-			"dog");
-
+	
 	@Test
-	public void testReactor1() {
-		Flux<String> fewWords = Flux.just("Hello", "World");
-		Flux<String> manyWords = Flux.fromIterable(words);
-
-		fewWords.subscribe(System.out::println);
-		System.out.println();
-		manyWords.subscribe(System.out::println);
+	public void test2() {
+		//操作测试时间
+		//需要验证的流中包含两个产生间隔为一天的元素，并且第一个元素的产生延迟是4个小时
+		//expectNoEvent()方法用来验证在 4 个小时之内没有任何消息产生
+		//然后验证第一个元素 0 产生
+		//接着thenAwait()方法来让虚拟时钟前进一天
+		//然后验证第二个元素 1 产生
+		//最后验证流正常结束
+		StepVerifier.withVirtualTime(() -> 
+				Flux.interval(Duration.ofHours(4), 
+						Duration.ofDays(1)).take(2))
+        	.expectSubscription()
+        	.expectNoEvent(Duration.ofHours(4))
+        	.expectNext(0L)
+        	.thenAwait(Duration.ofDays(1))
+        	.expectNext(1L)
+        	.verifyComplete();
 	}
-
+	
 	@Test
-	public void testReactor2() {
-		Flux.fromIterable(words).flatMap(word -> Flux.fromArray(word.split(""))).concatWith(Mono.just("s")).distinct()
-				.sort()
-				.zipWith(Flux.range(1, Integer.MAX_VALUE), (string, count) -> String.format("%2d. %s", count, string))
-				.subscribe(System.out::println);
-	}
-
-	@Test
-	public void testReactor3() {
-		Flux.range(1, 2).log("Range").subscribe(System.out::println);
-
-		StepVerifier.withVirtualTime(() -> Flux.interval(Duration.ofHours(4), Duration.ofDays(1)).take(2))
-				.expectSubscription().expectNoEvent(Duration.ofHours(4)).expectNext(0L).thenAwait(Duration.ofDays(1))
-				.expectNext(1L).verifyComplete();
-
-	}
-
-	@Test
-	public void testReactor4() {
-		StepVerifier.create(Flux.just(1, 3, 5, 2, 4, 6, 11, 12, 13).groupBy(i -> i % 2 == 0 ? "even" : "odd")
-				.concatMap(g -> g.defaultIfEmpty(-1) // if empty groups, show them
-						.map(String::valueOf) // map to string
-						.startWith(g.key())) // start with the group's key
-		).expectNext("odd", "1", "3", "5", "11", "13").expectNext("even", "2", "4", "6", "12").verifyComplete();
-		// Flux.range(1, 10).windowWhile(i -> i % 2 ==
-		// 0).concatMap(g->g.defaultIfEmpty(-1)).subscribe(System.out::println);
-
-		StepVerifier.create(Flux.range(1, 10).window(5, 3) // overlapping windows
-				.concatMap(g -> g.defaultIfEmpty(-1)) // show empty windows as -1
-		).expectNext(1, 2, 3, 4, 5).expectNext(4, 5, 6, 7, 8).expectNext(7, 8, 9, 10).expectNext(10).verifyComplete();
-
-		StepVerifier.create(Flux.range(1, 10).buffer(5, 3) // overlapping buffers
-		).expectNext(Arrays.asList(1, 2, 3, 4, 5)).expectNext(Arrays.asList(4, 5, 6, 7, 8))
-				.expectNext(Arrays.asList(7, 8, 9, 10)).expectNext(Collections.singletonList(10)).verifyComplete();
-
-		Flux.range(1, 10).parallel(2).subscribe(i -> System.out.println(Thread.currentThread().getName() + " -> " + i));
-
-		Flux.range(1, 10).parallel(2).runOn(Schedulers.parallel())
-				.subscribe(i -> System.out.println(Thread.currentThread().getName() + " -> " + i));
+	public void test3() {
+		//使用TestPublisher控制流中元素产生
+		final TestPublisher<String> testPublisher = TestPublisher.create();
+		testPublisher.next("a");
+		testPublisher.next("b");
+		testPublisher.complete();
+		 
+		StepVerifier.create(testPublisher)
+			.expectNext("a")
+		    .expectNext("b")
+		    .expectComplete();
 	}
 }
